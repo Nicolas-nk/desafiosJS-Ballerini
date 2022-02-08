c8afeaeb14f961fc3d5224e8ea9422ec1b88515d
Nicolas-nk <nicolas0102messias@gmail.com>
2022-02-08 12:01:22 -0300

    Desafio 1

---
+++ desafio1/scripts.js
@@ -0,0 +1,4 @@
+var nome=prompt('Olá! Qual o seu nome?');
+var idade=prompt('oi, ' + nome + '! Quanto anos você tem? :3');
+var linguagem=prompt('E qual linguagem você está estudando?');
+alert('Parabéns ' + nome + '! Você tem ' + idade + ' anos e já está aprendendo ' + linguagem + '! :D')
